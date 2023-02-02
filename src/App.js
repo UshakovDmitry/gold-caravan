3cf184148ebf4abb4dd37729b3c52e67d071f459
Dmitry Ushakov <dimau95@yandex.ru>
2023-02-02 20:59:25 +0600

    wr

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ import Header from './Header/Header.jsx'
 function App() {
   return (
     <>
+      <div className="page">
       <div className="main">
       <Header />
+
       <img className="main__img" src={mainImg} alt="Логотип" />
+      </div>
+
       </div>
     
     
