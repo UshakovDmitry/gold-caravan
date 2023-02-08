206672b4791b4374800be4110d93b42c53c6c566
Dmitry Ushakov <dimau95@yandex.ru>
2023-02-09 01:25:36 +0600

    ref Company component

--- src/components/Company/Company.js
+++ src/components/Company/Company.js
@@ -1,4 +1,6 @@
 import css from "./Company.module.css";
+import truck__1 from '../../images/main_img_1.jpg'
+import truck__2 from '../../images/main_img2.jpg'
 
 const Company = () => {
   return (
@@ -21,13 +23,27 @@ const Company = () => {
           <div className={css.midle}></div>
 
           <div className={css.grid}>
-            <div className={css.item_color}></div>
-            <div className={css.item_color}></div>
-            <div className={css.item}>
-                <img
+            <div className={css.item_color}>
+                <h3 className={css.h3}>571</h3>
+                <h4 className={css.h4}>заказ</h4>
+                <h5 className={css.h5}>между странами</h5>
             </div>
-            <div className={css.item}></div>
-            <div className={css.item_color}></div>
+            <div className={css.item_color}>
+            <h3 className={css.h3}>127</h3>
+                <h4 className={css.h4}>грузовиков</h4>
+                <h5 className={css.h5}>разный видов</h5>
+            </div>
+            <div className={css.item}>
+                <img className={css.img__inside} src ={truck__1} />
+            </div>
+            <div className={css.item}>
+            <img className={css.img__inside} src ={truck__2} />
+            </div>
+            <div className={css.item_color}>
+            <h3 className={css.h3}>61</h3>
+                <h4 className={css.h4}>водитель</h4>
+                <h5 className={css.h5}>профессионал</h5>
+            </div>
             <div className={css.last_item}></div>
           </div>
         </div>
